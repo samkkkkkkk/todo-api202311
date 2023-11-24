8d8838bf3f604e05e1041a413c1529f68c9144d2
samkyung <rnflrntk@naver.com>
2023-11-24 10:26:15 +0900

    #1. todo api 할 일 기능 작성 중 (create, select)

--- src/main/java/com/example/todo/todoapi/api/TodoController.java
+++ src/main/java/com/example/todo/todoapi/api/TodoController.java
@@ -1,11 +1,15 @@
 package com.example.todo.todoapi.api;
 
 
+import com.example.todo.todoapi.dto.request.TodoCreateRequestDTO;
+import com.example.todo.todoapi.dto.response.TodoListReponseDTO;
 import com.example.todo.todoapi.service.TodoService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @Slf4j
@@ -13,6 +17,47 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/todos")
 public class TodoController {
 
-    private TodoService todoService;
+    private final TodoService todoService;
+
+    @PostMapping
+    public ResponseEntity<?> createTodo(
+            @Validated @RequestBody TodoCreateRequestDTO requestDTO,
+            BindingResult result
+    ) {
+        if(result.hasErrors()){
+            log.warn("DTO 검증 에러 발생: {}", result.getFieldError());
+            return ResponseEntity
+                    .badRequest()
+                    .body(result.getFieldError());
+        }
+
+        try {
+            TodoListReponseDTO responseDTO = todoService.create(requestDTO);
+            return ResponseEntity
+                    .ok()
+                    .body(responseDTO);
+        } catch (RuntimeException e) {
+            /*log.info(e.getMessage());*/
+            e.printStackTrace();
+            return ResponseEntity
+                    .internalServerError()
+                    .body(TodoListReponseDTO
+                            .builder()
+                            .error(e.getMessage())
+                            .build());
+
+        }
+
+    }
+
+    // 할 일 목록 요청
+    @GetMapping
+    public  ResponseEntity<?> retrieveTodoList() {
+        log.info("/api/todos GET request");
+        TodoListReponseDTO responseDTO = todoService.retrieve();
+
+        return ResponseEntity.ok().body(responseDTO);
+    }
+
 
 }
--- src/main/java/com/example/todo/todoapi/service/TodoService.java
+++ src/main/java/com/example/todo/todoapi/service/TodoService.java
@@ -1,10 +1,17 @@
 package com.example.todo.todoapi.service;
 
+import com.example.todo.todoapi.dto.request.TodoCreateRequestDTO;
+import com.example.todo.todoapi.dto.response.TodoDetailResponseDTO;
+import com.example.todo.todoapi.dto.response.TodoListReponseDTO;
+import com.example.todo.todoapi.entity.Todo;
 import com.example.todo.todoapi.repository.TodoRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @Slf4j
@@ -13,5 +20,28 @@ import org.springframework.transaction.annotation.Transactional;
 public class TodoService {
 
     private final TodoRepository todoRepository;
+
+    public TodoListReponseDTO create(final TodoCreateRequestDTO requestDTO)
+            throws RuntimeException {
+        todoRepository.save(requestDTO.toEntity());
+        log.info("할 일 저장 완료! 제목: {}", requestDTO.getTitle());
+
+        return retrieve();
+
+    }
+
+    public TodoListReponseDTO retrieve() {
+        List<Todo> entityList = todoRepository.findAll();
+
+        List<TodoDetailResponseDTO> dtoList = entityList.stream()
+                /*.map(todo -> new TodoDetailResponseDTO(todo))*/
+                .map(TodoDetailResponseDTO::new)
+                .collect(Collectors.toList());
+
+        return TodoListReponseDTO.builder()
+                .todos(dtoList)
+                .build();
+    }
+
 
 }
